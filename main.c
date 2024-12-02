242599a58372c7c3206ec478fc2d4a1f46434231
Stefano-di-flavia <stefanotindaro.diflavia@davincimilazzo.edu.it>
2024-12-02 09:03:46 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include"math.c"
+#include"math.h"
+
+int main()
+int scelta, a, b;
+float NumberA, NumberB;
+float Risultato;
+{
+    printf("scegli l'operazione che desidera fare:\n");
+    printf("1. somma\n");
+    printf("2. moltiplicazione\n");
+    printf("3. differenza\n");
+    printf("4. divisione\n");
+    printf("5. potenza\n");
+    scanf("%d" &scelta);
+
+    switch (scelta){
+        case 1: 
+        printf("inserisci il primo numero:\n");
+        scanf("%d")
+        printf
+
+    }
+}
---
+++ math.c
@@ -0,0 +1,16 @@
+#include "math.h"
+int somma(int a, int b){
+    return a + b;
+}
+
+int differenza(int a, int b){
+    return a - b;
+}
+
+int moltiplicazione(int a, int b){
+    return a * b;
+}
+
+int divisione(float a, float b){
+    if 
+}
